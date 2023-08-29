72f256831073810d963073e7cdc11d8a08d0eced
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-08-29 18:19:49 +0200

    backend

---
+++ db/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crmvpgv.mongodb.net/?retryWrites=true&w=majority`)
+    .then(response => console.log("reussie ! 🥳"))
+    .catch(err => console.log("erreur", err))
+
+
+
+
+
