87cd483ba3bd74973c87f6888e4f4e142102a770
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-08-29 18:48:52 +0200

    back

--- db/db.js
+++ db/db.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+require("dotenv").config()
+
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crmvpgv.mongodb.net/?retryWrites=true&w=majority`)
     .then(response => console.log("reussie ! 🥳"))
